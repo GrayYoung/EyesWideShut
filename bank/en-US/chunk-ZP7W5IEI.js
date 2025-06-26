2412324d57bf9b16ca9f4401c5c70d76ad9286ce
GrayYoung <windpet@live.cn>
2025-06-26 03:24:36 +0000

    Automatically deploy Bank project. https://github.com/GrayYoung/Bank/commit/99170af11feddf9f9f5a6a216fa9bd76d216b30a

--- bank/en-US/chunk-ZP7W5IEI.js
+++
@@ -1,2 +0,0 @@
-import{Bd as i,ja as t,ta as e}from"./chunk-V2NSBNR3.js";import"./chunk-XGK45UGL.js";import"./chunk-NQUV66Z4.js";var a=(()=>{class o{static \u0275fac=function(r){return new(r||o)};static \u0275mod=e({type:o});static \u0275inj=t({imports:[i]})}return o})();export{a as CFEModule};
-/**i18n:4ec78395217facedd742edef8dfa814b1ea6af3fbb33fa1f9a497a05293d0698*/
---
+++ bank/zh/chunk-DKAKLER6.js
@@ -0,0 +1,2 @@
+import"./chunk-RAFVY6Q4.js";import"./chunk-UC2JMEVD.js";import"./chunk-6B763VMG.js";import"./chunk-LO4PBWZP.js";
+/**i18n:4e954d11e025817f1aceae10dbbd8d111274144b3c3dbbdb81deac1f9b9c7958*/
